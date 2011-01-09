f0c8bbeca54608ecae2de01359dfdcc54413f932
liwei <hi@liwei.me>
2011-01-09 14:23:44 +0800

    nav ui

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(:version => 20110104093702) do
 
   create_table "emails", :force => true do |t|
     t.string   "address"
-    t.datetime "last_send"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,3 @@
-function clearText(theField)
-{
-  if (theField.defaultValue == theField.value)
-    theField.value = '';
-}
-
-function addText(theField)
-{
-  if (theField.value == '')
-    theField.value = theField .defaultValue;
-}
-
 (function($){
 
 	$(document).ready(function(){
